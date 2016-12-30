c3fa28936182825f26f5ba66ef8e9a8df6e7f027
Jared Hanson <jaredhanson@gmail.com>
2016-12-30 15:45:04 -0800

    Fix tests.

--- test/ds/users/authenticators.test.js
+++ test/ds/users/authenticators.test.js
@@ -2,7 +2,7 @@
 
 var expect = require('chai').expect;
 var sinon = require('sinon');
-var factory = require('../../../xom/ds/users/authenticators');
+var factory = require('../../../lib/ds/users/authenticators');
 
 
 describe('authy/ds/credentials', function() {
