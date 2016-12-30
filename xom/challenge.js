025f6b0e031fdcff1fad2d734283c3acc54330de
Jared Hanson <jaredhanson@gmail.com>
2016-12-30 15:38:27 -0800

    Simplify challenge.

--- xom/challenge.js
+++ xom/challenge.js
@@ -1,8 +1,28 @@
 exports = module.exports = function(idmap, client) {
-  // Load modules.
-  var challenge = require('../lib/challenge');
   
-  return challenge(client, idmap);
+  return function challenge(user, credID, options, cb) {
+    if (typeof options == 'function') {
+      cb = options;
+      options = undefined;
+    }
+    options = options || {};
+    
+    // TODO: Other OTP transport mechanisms
+    /*
+    //authy.request_sms(authyID, true, function (err, res) {
+    //authy.request_call(authyID, function (err, res) {
+    //authy.request_call(authyID, true, function (err, res) {
+    */
+    
+    idmap(user, function(err, authyID) {
+      if (err) { return cb(err); }
+    
+      client.request_sms(authyID, function(err, res) {
+        if (err) { return cb(err); }
+        cb();
+      });
+    });
+  };
 };
 
 exports['@implements'] = [
