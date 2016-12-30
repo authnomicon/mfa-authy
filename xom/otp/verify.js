44f27c92c4c6d871c66beadb10b2e2454e9fcc29
Jared Hanson <jaredhanson@gmail.com>
2016-12-30 15:39:42 -0800

    Simplify verify.

--- xom/otp/verify.js
+++ xom/otp/verify.js
@@ -1,8 +1,30 @@
 exports = module.exports = function(idmap, client) {
-  // Load modules.
-  var verify = require('../../lib/verify');
   
-  return verify(client, idmap);
+  return function verify(user, credID, otp, options, cb) {
+    if (typeof options == 'function') {
+      cb = options;
+      options = undefined;
+    }
+    options = options || {};
+    
+    
+    idmap(user, function(err, authyID) {
+      if (err) { return cb(err); }
+      
+      client.verify(authyID, otp, function(err, res) {
+        if (err) {
+          if (err.success == false && err.token == 'is invalid') {
+            return cb(null, false);
+          }
+          return cb(err);
+        }
+
+        if (res.success == 'true' && res.token == 'is valid') {
+          return cb(null, true);
+        }
+      });
+    });
+  };
 };
 
 exports['@implements'] = [
