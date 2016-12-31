b38bdcc702b247dba476a2319d41c70c27aa5218
Jared Hanson <jaredhanson@gmail.com>
2016-12-31 11:22:57 -0800

    Add test cases for OOB authentication.

---
+++ test/oob/verify.test.js
@@ -0,0 +1,131 @@
+/* global describe, it, expect */
+
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var factory = require('../../lib/oob/verify');
+
+
+describe('oob/verify', function() {
+  
+  it('should export factory function', function() {
+    expect(factory).to.be.a('function');
+  });
+  
+  it('should be annotated', function() {
+    expect(factory['@implements']).to.have.length(2);
+    expect(factory['@implements'][0]).to.equal('http://schemas.authnomicon.org/js/login/mfa/oob/verify');
+    expect(factory['@implements'][1]).to.equal('http://schemas.authnomicon.org/js/login/mfa/opt/authy/oob/verify');
+    expect(factory['@singleton']).to.equal(true);
+  });
+  
+  describe('verify', function() {
+    var client = {
+      verify: function(){}
+    };
+  
+    
+    describe('a valid token received via SMS', function() {
+      var ok;
+      
+      before(function() {
+        var result = {
+          message: 'Token is valid.',
+          token: 'is valid',
+          success: 'true'
+        };
+        
+        sinon.stub(client, 'verify').yields(null, result);
+      });
+    
+      after(function() {
+        client.verify.restore();
+      });
+      
+      before(function(done) {
+        var verify = factory(client);
+        var authenticator = {
+          id: '0',
+          type: [ 'otp', 'oob' ],
+          _userID: 123456
+        }
+        var opts = {
+          secret: '1234567',
+          context: {}
+        }
+        
+        verify(authenticator, undefined, opts, function(_err, _ok) {
+          if (_err) { return done(_err); }
+          ok = _ok;
+          done();
+        });
+      });
+    
+      it('should verify token', function() {
+        expect(client.verify).to.have.been.calledOnce;
+        var call = client.verify.getCall(0);
+        expect(call.args[0]).to.equal(123456);
+        expect(call.args[1]).to.equal('1234567');
+      });
+      
+      it('should yield ok', function() {
+        expect(ok).to.be.true;
+      });
+      
+    }); // a valid token
+    
+    describe('an invalid token', function() {
+      var ok;
+      
+      before(function() {
+        var result = {
+          message: 'Token is invalid',
+          token: 'is invalid',
+          success: false,
+          errors: {
+            message: 'Token is invalid'
+          },
+          error_code: '60020'
+        };
+        
+        sinon.stub(client, 'verify').yields(result);
+      });
+    
+      after(function() {
+        client.verify.restore();
+      });
+      
+      before(function(done) {
+        var verify = factory(client);
+        var authenticator = {
+          id: '0',
+          type: [ 'otp', 'oob' ],
+          _userID: 123456
+        }
+        var opts = {
+          secret: '1234567',
+          context: {}
+        }
+        
+        verify(authenticator, undefined, opts, function(_err, _ok) {
+          if (_err) { return done(_err); }
+          ok = _ok;
+          done();
+        });
+      });
+    
+      it('should verify token', function() {
+        expect(client.verify).to.have.been.calledOnce;
+        var call = client.verify.getCall(0);
+        expect(call.args[0]).to.equal(123456);
+        expect(call.args[1]).to.equal('1234567');
+      });
+      
+      it('should yield ok', function() {
+        expect(ok).to.be.false;
+      });
+      
+    }); // an invalid token
+    
+  }); // verify
+  
+});
